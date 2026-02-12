e2bb7310923ca62a6a9eeede5d5a6fb3468a43bd
sloan05 <musicmonster127@gmail.com>
2026-02-12 14:37:12 -0800

    Revert "HW4"
    This reverts commit 9c0fbca949516cc7ec1ef6b3ce96930888593c17.

--- MG4_2026/Assets/Scripts/Audio.cs
+++ MG4_2026/Assets/Scripts/Audio.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InvisableWall : MonoBehaviour
+public class Audio : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
